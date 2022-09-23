3485c7c40206df7a305d0375a0b923170f20dcaf
dinixweb <sd4live@gmail.com>
2022-09-23 23:11:20 +0100

    first commit

---
+++ src/main/java/dinixweb/io/VideoStreamingService/VideoStreamingServiceApplication.java
@@ -0,0 +1,13 @@
+package dinixweb.io.VideoStreamingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoStreamingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoStreamingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dinixweb/io/VideoStreamingService/VideoStreamingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dinixweb.io.VideoStreamingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStreamingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
